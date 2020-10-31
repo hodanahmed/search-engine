16888617ca0b141b8fbe6e24c587f946caf2c90f
Hodan <hodanahmed00@gmail.com>
2020-10-31 09:45:09 +0000

    created an ajax request using axios to UnSplash api

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,25 @@ import React from 'react';
 import axios from 'axios';
 import SearchBar from './SearchBar';
 class App extends React.Component {
-	onSearchSubmit(input) {
-		console.log(input)
+	state = { images: [] };
+	 onSearchSubmit = async (input) => {
+		// Documentation states that the end point of Unsplahs API is rootURL/search/photos
+		const response = await axios.get('https://api.unsplash.com/search/photos', {
+			params: {query: input},
+			headers: {
+				Authorization: `Client-ID brtn-_hIldVnz5PQ6ndsQuyEw1TcZGAxwzTMFdwGswk`
+			}
+		})
+			// let data = response.data.results
+			// data.forEach(element => console.log(element.urls))
+			// return (<img alt="these are things" src={response.data.results[0].urls.raw}/>)
+		this.setState({ images: response.data.results })
 	}
-
 	render() {
 		return (
 			<div className="ui container" style={{ marginTop: "10px" }}>
-				<SearchBar onSubmit={this.onSearchSubmit}/>
+				<SearchBar onSubmit={this.onSearchSubmit} />
+				Found: {this.state.images.length} images
 			</div>
 		)
 	};
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,14 +4,9 @@ class SearchBar extends React.Component {
 	state = {
 		input: "",
     }
-    // Using an arrow function binds 'this' with the function so I no longer have that 
-    // the error that says "cant find state of undefinied"
     onFormSubmit = (e) => {
-        // Prevents default submission usually html event stuff
 		e.preventDefault();
-		// the onSubmit function was passed a prop from App.js. to call it, use 'this.props.onSubmit'
-		// it takes the parameter 'this.state.input'. In the app, it console.logs the parameter.
-       this.props.onSubmit(this.state.input)
+	   this.props.onSubmit(this.state.input)
     }
 	render() {
 		return (
@@ -19,11 +14,9 @@ class SearchBar extends React.Component {
 				<form onSubmit={this.onFormSubmit} className="ui form">
 					<div className="field">
 						<label>Image search</label>
-						{/* onChange is an event handler that will run when the input change. */}
 						<input
 							type="text"
 							placeholder="Enter text"
-							// We set the value as the current value of the input in order to have a controlled form element.
 							value={this.state.input}
 							onChange={(e) => this.setState({ input: e.target.value })}
 						/>
